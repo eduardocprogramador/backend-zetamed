3d363faf2c80d84a1d215e40060b1b4a649dc704
edu22 <edu22@gmail.com>
2025-10-23 11:37:46 -0300

    adicionado rota /

--- index.js
+++ index.js
@@ -5,6 +5,12 @@ const app = express()
 app.use(express.json())
 app.use(cors({credentials: true, origin: process.env.ORIGIN}))
 app.use(express.static('public'))
+app.get('/', (req, res) => {
+  res.status(200).json({
+    message: '✅ API está funcionando!',
+    status: 'ok'
+  })
+})
 const EmailRoutes = require('./routes/EmailRoutes')
 app.use('/email', EmailRoutes)
 const PORT = process.env.PORT || 5000
