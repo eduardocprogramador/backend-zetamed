d3c1bcf8522dac665696f867b250d4b0c35e9a0f
eduardocprogramador <eduardocostacarvalho7@gmail.com>
2026-06-01 16:43:51 -0300

    commit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const cors = require('cors')
 const app = express()
 app.use(express.json())
-app.use(cors({credentials: true, origin: process.env.ORIGIN}))
+app.use(cors(/*{credentials: true, origin: process.env.ORIGIN}*/))
 app.use(express.static('public'))
 app.get('/', (req, res) => {
   res.status(200).json({
